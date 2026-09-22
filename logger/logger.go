e22a530b7b1ce6283bf399cfac5ec8ca953cabe5
khotiburrahman <64509042+khotiburrahman@users.noreply.github.com>
2026-09-22 18:10:56 +0700

    Update logger.go

--- logger/logger.go
+++ logger/logger.go
@@ -1,43 +1,54 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 var (
 	Enable      = true
 	DebugEnable = false
 )
 
+func ts() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
 func emit(msg string) {
 	if !Enable {
 		return
 	}
-	fmt.Println(msg)
+	fmt.Printf("[%s] %s\n", ts(), msg)
 }
 
+// Debug hanya tampil jika Enable dan DebugEnable bernilai true.
 func Debug(format string, a ...any) {
 	if !Enable || !DebugEnable {
 		return
 	}
-	fmt.Printf("[DEBUG] "+format+"\n", a...)
+	fmt.Printf("[%s] [DEBUG] "+format+"\n", append([]any{ts()}, a...)...)
 }
 
+// Info selalu tampil jika Enable true.
 func Info(format string, a ...any) {
 	if !Enable {
 		return
 	}
-	fmt.Printf(format+"\n", a...)
+	fmt.Printf("[%s] "+format+"\n", append([]any{ts()}, a...)...)
 }
 
+// Warn selalu tampil jika Enable true.
 func Warn(format string, a ...any) {
 	if !Enable {
 		return
 	}
-	fmt.Printf("[WARN] "+format+"\n", a...)
+	fmt.Printf("[%s] [WARN] "+format+"\n", append([]any{ts()}, a...)...)
 }
 
+// Errorf selalu tampil jika Enable true.
 func Errorf(format string, a ...any) {
 	if !Enable {
 		return
 	}
-	fmt.Printf("[ERROR] "+format+"\n", a...)
+	fmt.Printf("[%s] [ERROR] "+format+"\n", append([]any{ts()}, a...)...)
 }
